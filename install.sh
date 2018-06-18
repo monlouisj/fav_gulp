eb97b5209ec9cc50792c4372d240c98dc0f774d5
monlouisj <nelojbrown@gmail.com>
2018-06-18 11:49:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npm i browserify gulp gulp-autoprefixer gulp-clean-css gulp-concat gulp-google-fonts gulp-image-resize gulp-livereload gulp-modify-file gulp-rename gulp-sass gulp-serve gulp-svgmin gulp-uglify vinyl-buffer vinyl-source-stream --save-dev
+npm i browserify node-sass gulp gulp-autoprefixer gulp-clean-css gulp-concat gulp-google-fonts gulp-image-resize gulp-livereload gulp-modify-file gulp-rename gulp-sass gulp-serve gulp-svgmin gulp-uglify vinyl-buffer vinyl-source-stream --save-dev
 npm i jquery bootstrap imagesloaded lodash text-spinners --save
